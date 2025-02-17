08fa1ba845a2e1b9ccaa1d1e12ac08d1811b7dc3
#V0ID <hey@v0id.me>
2025-02-17 04:24:18 +0300

    chore: add development tools and configuration

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [0], // Allow both with and without scope
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "perf",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+  },
+};
