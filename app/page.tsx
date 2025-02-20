3eab24c175e4808e997e2e78969950106e13cc71
#V0ID <hey@v0id.me>
2025-02-20 18:34:29 +0300

    feat: add Zustand store and section selection logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+"use client";
+
+import { useSelectionStore } from "@/store/selection";
+import { lazy } from "react";
+import Section from "@/enums/selection";
+const ColorBanner = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <>
+      <div className="w-full h-10 bg-[#DC0E11]"></div>
+    </>
+  );
+};
+
+const Chats = lazy(() => import("./Chats"));
+export default function Home() {
+  const { selectedSection } = useSelectionStore();
+
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <ColorBanner />
+      {/* lazy loading */}
+      {selectedSection === Section.Chats && <Chats />}
     </main>
   );
 }
---
+++ store/selection/selection.ts
@@ -0,0 +1,15 @@
+/*
+ * This is used to managed selected UI section
+ */
+import { create } from "zustand";
+import Section from "@/enums/selection";
+
+interface SelectionStore {
+  selectedSection: Section;
+  setSelectedSection: (section: Section) => void;
+}
+
+export const useSelectionStore = create<SelectionStore>((set) => ({
+  selectedSection: Section.Chats,
+  setSelectedSection: (section) => set({ selectedSection: section }),
+}));
