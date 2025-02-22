89330d55b25e1efd5a5ae45aac9bd801fdb353b0
#V0ID <hey@v0id.me>
2025-02-22 21:51:40 +0300

    feat: improve Chats and SearchBar components with enhanced UI and props

--- app/Chats/index.tsx
+++ app/Chats/index.tsx
@@ -1,10 +1,17 @@
 import SearchBar from '@/components/utils/SearchBar';
-
+import ThreeDots from '@/public/icons/dots-three-outline-vertical.svg';
+import UserPlus from '@/public/icons/user-plus.svg';
 export default function Chats() {
   return (
-    <div className="flex flex-col h-full p-12 border border-black">
-      <h1 className="text-2xl font-bold">محادثات</h1>
-      <SearchBar />
+    <div className="flex flex-col h-full p-12 border-l border-t border-b border-black bg-white">
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-bold">محادثات</h1>
+        <div className="flex items-center gap-4">
+          <UserPlus className="w-8 h-8 fill-[#6B6060] hover:fill-[#000] hover:bg-gray-100 rounded-full cursor-pointer p-1" />
+          <ThreeDots className="w-8 h-8 fill-[#6B6060] hover:fill-[#000] hover:bg-gray-100 rounded-full cursor-pointer p-1" />
+        </div>
+      </div>
+      <SearchBar placeholder="ابحث عن محادثة" className="mt-4" />
     </div>
   );
 }
--- components/utils/SearchBar.tsx
+++ components/utils/SearchBar.tsx
@@ -1,16 +1,21 @@
 import MagnifyingGlass from '@/public/icons/magnifying-glass.svg';
 
-export default function SearchBar() {
+interface SearchBarProps {
+  placeholder?: string;
+  className?: string;
+}
+
+export default function SearchBar({ placeholder = 'بحث', className }: SearchBarProps) {
   return (
-    <div className="flex items-center mx-auto w-full px-4 lg:w-2/3 lg:px-0">
+    <div className={`flex items-center mx-auto w-full ${className}`}>
       <div className="relative w-full">
         <div className="absolute right-3 top-1/2 -translate-y-1/2">
-          <MagnifyingGlass className="fill-[#565656]" />
+          <MagnifyingGlass className="fill-[#565656] w-5 h-5" />
         </div>
         <input
           type="text"
-          placeholder="بحث"
-          className="p-2 w-full rounded-full border border-[#C8C8C8] bg-[#A7A7A7] text-[#565656] focus:outline-none pr-10 text-right placeholder-[#565656]"
+          placeholder={placeholder}
+          className="w-full py-2 px-4 pr-10 rounded-full border border-[#C8C8C8] bg-[#A7A7A7] text-[#565656] focus:outline-none text-right placeholder-[#565656]"
         />
       </div>
     </div>
