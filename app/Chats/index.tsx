6770028fa34b123e7b9fe72c1a2950f8957d22a1
#V0ID <hey@v0id.me>
2025-02-22 20:44:16 +0300

    feat: add SVG support, component previews, and enhance SearchBar component

--- app/Chats/index.tsx
+++ app/Chats/index.tsx
@@ -1,7 +1,10 @@
+import SearchBar from '@/components/utils/SearchBar';
+
 export default function Chats() {
   return (
     <div className="flex flex-col h-full p-12 border border-black">
       <h1 className="text-2xl font-bold">محادثات</h1>
+      <SearchBar />
     </div>
   );
 }
--- components/utils/SearchBar.tsx
+++ components/utils/SearchBar.tsx
@@ -1,3 +1,18 @@
+import MagnifyingGlass from '@/public/icons/magnifying-glass.svg';
+
 export default function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <div className="flex items-center mx-auto w-full px-4 lg:w-2/3 lg:px-0">
+      <div className="relative w-full">
+        <div className="absolute right-3 top-1/2 -translate-y-1/2">
+          <MagnifyingGlass className="fill-[#565656]" />
+        </div>
+        <input
+          type="text"
+          placeholder="بحث"
+          className="p-2 w-full rounded-full border border-[#C8C8C8] bg-[#A7A7A7] text-[#565656] focus:outline-none pr-10 text-right placeholder-[#565656]"
+        />
+      </div>
+    </div>
+  );
 }
